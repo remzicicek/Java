52979a6dd9bf50e25cb299c8dbefd1a14c65cbeb
remzicicek <remzicicek61@gmail.com>
2021-11-20 15:46:44 +0300

    base model is updated

--- springboot/mapstruct/src/main/java/com/mapstruct/example/mapstruct/model/BaseModel.java
+++ springboot/mapstruct/src/main/java/com/mapstruct/example/mapstruct/model/BaseModel.java
@@ -1,15 +1,9 @@
 package com.mapstruct.example.mapstruct.model;
 
-import com.mapstruct.example.mapstruct.constants.BaseConstant;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-
 import javax.persistence.*;
 import java.util.Date;
 
